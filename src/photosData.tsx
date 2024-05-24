ab0aeb6c3cb5335fb55df18234acc43a0b3e5574
Sam Swarr <swarr687@gmail.com>
2024-05-24 18:25:46 -0400

    add more artwork

--- src/photosData.tsx
+++ src/photosData.tsx
@@ -1,6 +1,45 @@
+import halmonis_house_20_16 from "./assets/halmonis_house_20_16.jpeg";
+import miyajima_2_15_11 from "./assets/miyajima_2_15_11.jpg";
+import sunday_afternoon_16_20 from "./assets/sunday_afternoon_16_20.jpeg";
+import vernazza_5_7 from "./assets/vernazza_5_7.jpg";
 import mushroom_1 from "./assets/mushroom_1.jpeg";
 import mushroom_2 from "./assets/mushroom_2.jpeg";
 import mushroom_3 from "./assets/mushroom_3.jpeg";
+import blueberries_16_12 from "./assets/blueberries_16_12.jpg";
+import babble_20_16 from "./assets/babble_20_16.jpg";
+import dreamscape_20_16 from "./assets/dreamscape_20_16.jpg";
+import alishan_8_8 from "./assets/alishan_8_8.jpg";
+import taroko_8_8 from "./assets/taroko_8_8.jpg";
+import beitou_8_8 from "./assets/beitou_8_8.jpg";
+import pears_16_20 from "./assets/pears_16_20.jpeg";
+import queens_boulevard_20_16 from "./assets/queens_boulevard_20_16.jpg";
+import perezoso_11_15 from "./assets/perezoso_11_15.jpg";
+import gone_fishing_7_5 from "./assets/gone_fishing_7_5.jpg";
+import ricketts_glen_7_5 from "./assets/ricketts_glen_7_5.jpg";
+import winter_morning_7_5 from "./assets/winter_morning_7_5.jpg";
+import christmas_morning_7_5 from "./assets/christmas_morning_7_5.jpg";
+import ice_fisher_7_5 from "./assets/ice_fisher_7_5.jpg";
+import terrace_club_hopper_16_20 from "./assets/terrace_club_hopper_16_20.jpeg";
+import jersey_city_sunset_16_8 from "./assets/jersey_city_sunset_16_8.jpg";
+import prairie_storm_16_12 from "./assets/prairie_storm_16_12.jpg";
+import watertower_12_16 from "./assets/watertower_12_16.jpg";
+import fenchihu_8_8 from "./assets/fenchihu_8_8.jpg";
+import spoonbill_8_8 from "./assets/spoonbill_8_8.jpg";
+import mountain_hike_8_8 from "./assets/mountain_hike_8_8.jpg";
+import lotus_16_20 from "./assets/lotus_16_20.jpeg";
+import lovely_bride_16_20 from "./assets/lovely_bride_16_20.jpg";
+import mackerel_cove_7_5 from "./assets/mackerel_cove_7_5.jpg";
+import maine_cabin_20_16 from "./assets/maine_cabin_20_16.jpg";
+import meadowlands_16_12 from "./assets/meadowlands_16_12.jpg";
+import miyajima_16_20 from "./assets/miyajima_16_20.jpeg";
+import motmot_16_20 from "./assets/motmot_16_20.jpeg";
+import northern_lights_8_10 from "./assets/northern_lights_8_10.jpeg";
+import hanalei_30_22 from "./assets/hanalei_30_22.jpeg";
+import soho_8_8 from "./assets/soho_8_8.jpg";
+import julie_watches_the_sunset_12_16 from "./assets/julie_watches_the_sunset_12_16.jpg";
+import turks_7_5 from "./assets/turks_7_5.jpg";
+import valley_forge_8_8 from "./assets/valley_forge_8_8.jpg";
+import city_sunset_20_16 from "./assets/city_sunset_20_16.jpeg";
 import picos from "./assets/picos.jpeg";
 
 export type PhotoData = {
@@ -12,6 +51,34 @@ export type PhotoData = {
 };
 
 export const photosData: PhotoData[] = [
+  {
+    src: halmonis_house_20_16,
+    title: '"Halmoni\'s House"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 600,
+  },
+  {
+    src: miyajima_2_15_11,
+    title: '"Miyajima 2"',
+    subtitle: 'Watercolor — 15" x 11"',
+    width: 800,
+    height: 567,
+  },
+  {
+    src: sunday_afternoon_16_20,
+    title: '"Sunday Afternoon"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 637,
+    height: 800,
+  },
+  {
+    src: vernazza_5_7,
+    title: '"Vernazza"',
+    subtitle: 'Watercolor — 5" x 7"',
+    width: 557,
+    height: 800,
+  },
   {
     src: mushroom_1,
     title: '"Porcini"',
@@ -32,6 +99,251 @@ export const photosData: PhotoData[] = [
     subtitle: 'Watercolor — 8" x 10"',
     width: 629,
     height: 800,
+  },
+  {
+    src: blueberries_16_12,
+    title: '"Blueberries"',
+    subtitle: 'Watercolor — 16" x 12"',
+    width: 800,
+    height: 600,
+  },
+  {
+    src: babble_20_16,
+    title: '"Babble"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 643,
+  },
+  {
+    src: dreamscape_20_16,
+    title: '"Dreamscape"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 640,
+  },
+  {
+    src: alishan_8_8,
+    title: '"Alishan"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 796,
+    height: 800,
+  },
+  {
+    src: taroko_8_8,
+    title: '"Taroko"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 799,
+    height: 800,
+  },
+  {
+    src: beitou_8_8,
+    title: '"Beitou"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 797,
+    height: 800,
+  },
+  {
+    src: pears_16_20,
+    title: '"Pears"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 640,
+    height: 800,
+  },
+  {
+    src: queens_boulevard_20_16,
+    title: '"Queens Boulevard"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 633,
+  },
+  {
+    src: perezoso_11_15,
+    title: '"Perezoso"',
+    subtitle: 'Watercolor — 11" x 15"',
+    width: 640,
+    height: 800,
+  },
+  {
+    src: gone_fishing_7_5,
+    title: '"Gone Fishing"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 578,
+  },
+  {
+    src: ricketts_glen_7_5,
+    title: '"Ricketts Glen"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 555,
+  },
+  {
+    src: winter_morning_7_5,
+    title: '"Winter Morning"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 564,
+  },
+  {
+    src: christmas_morning_7_5,
+    title: '"Christmas Morning"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 565,
+  },
+  {
+    src: ice_fisher_7_5,
+    title: '"Ice Fisher"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 569,
+  },
+  {
+    src: terrace_club_hopper_16_20,
+    title: '"Terrace Club Hopper"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 650,
+    height: 800,
+  },
+  {
+    src: jersey_city_sunset_16_8,
+    title: '"Jersey City Sunset"',
+    subtitle: 'Watercolor — 16" x 8"',
+    width: 800,
+    height: 404,
+  },
+  {
+    src: prairie_storm_16_12,
+    title: '"Prairie Storm"',
+    subtitle: 'Watercolor — 16" x 12"',
+    width: 800,
+    height: 642,
+  },
+  {
+    src: watertower_12_16,
+    title: '"Watertower"',
+    subtitle: 'Watercolor — 12" x 16"',
+    width: 602,
+    height: 800,
+  },
+  {
+    src: fenchihu_8_8,
+    title: '"Fenchihu"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 800,
+    height: 787,
+  },
+  {
+    src: spoonbill_8_8,
+    title: '"Spoonbill"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 800,
+    height: 783,
+  },
+  {
+    src: mountain_hike_8_8,
+    title: '"Mountain Hike"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 787,
+    height: 800,
+  },
+  {
+    src: lotus_16_20,
+    title: '"Lotus"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 637,
+    height: 800,
+  },
+  {
+    src: lovely_bride_16_20,
+    title: '"Lovely Bride"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 640,
+    height: 800,
+  },
+  {
+    src: mackerel_cove_7_5,
+    title: '"Mackerel Cove"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 550,
+  },
+  {
+    src: maine_cabin_20_16,
+    title: '"Maine Cabin"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 600,
+  },
+  {
+    src: meadowlands_16_12,
+    title: '"Meadowlands"',
+    subtitle: 'Watercolor — 16" x 12"',
+    width: 800,
+    height: 610,
+  },
+  {
+    src: miyajima_16_20,
+    title: '"Miyajima 1"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 800,
+    height: 589,
+  },
+  {
+    src: motmot_16_20,
+    title: '"Motmot"',
+    subtitle: 'Watercolor — 16" x 20"',
+    width: 640,
+    height: 800,
+  },
+  {
+    src: northern_lights_8_10,
+    title: '"Northern Lights"',
+    subtitle: 'Watercolor — 8" x 10"',
+    width: 583,
+    height: 800,
+  },
+  {
+    src: hanalei_30_22,
+    title: '"Hanalei"',
+    subtitle: 'Watercolor — 30" x 22"',
+    width: 800,
+    height: 591,
+  },
+  {
+    src: soho_8_8,
+    title: '"Soho"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 800,
+    height: 776,
+  },
+  {
+    src: julie_watches_the_sunset_12_16,
+    title: '"Julie Watches The Sunset"',
+    subtitle: 'Watercolor — 12" x 16"',
+    width: 602,
+    height: 800,
+  },
+  {
+    src: turks_7_5,
+    title: '"Turks"',
+    subtitle: 'Watercolor — 7" x 5"',
+    width: 800,
+    height: 554,
+  },
+  {
+    src: valley_forge_8_8,
+    title: '"Valley Forge"',
+    subtitle: 'Watercolor — 8" x 8"',
+    width: 784,
+    height: 800,
+  },
+  {
+    src: city_sunset_20_16,
+    title: '"City Sunset"',
+    subtitle: 'Watercolor — 20" x 16"',
+    width: 800,
+    height: 628,
   },
   {
     src: picos,
